3c0fd685fa71e59d35c6772ffb13ccf410d95554
vxni-23 <83010584+vxni-23@users.noreply.github.com>
2021-04-23 02:00:55 +0530

    Add files via upload

--- madlib_m.py
+++ madlib_m.py
@@ -180,25 +180,6 @@ def madlib2():
     sub_btn.grid(row = 9, column = 2)
 
 
-
-
-
-
-
-    adjactive = input('enter adjective : ')
-    color = input('enter a color name : ')
-    thing = input('enter a thing name :')
-    place = input('enter a place name : ')
-    person= input('enter a person name : ')
-    adjactive1 = input('enter a adjactive : ')
-    insect= input('enter a insect name : ')
-    food = input('enter a food name : ')
-    verb = input('enter a verb name : ')
-
-    print('Last night I dreamed I was a ' +adjactive+ ' butterfly with ' + color+ ' splocthes that looked like '+thing+ ' .I flew to ' + place+ ' with my bestfriend and '+person+ ' who was a '+adjactive1+ ' ' +insect +' .We ate some ' +food+ ' when we got there and then decided to '+verb+ ' and the dream ended when I said-- lets ' +verb+ '.')
-
-
-
 def madlib3():
 
     r1=Tk()
